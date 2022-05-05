68e922b50fca4a702d0616586140011959ae7c2e
Rana Arju <arjurana530@gmail.com>
2022-05-05 18:31:20 +0600

    new product add on client site and  this product set mongodb database this functionality added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const cors = require("cors");
@@ -16,29 +16,41 @@ const run = async () =>{
 await client.connect();
   const logicaCollection = client.db("logicaProduct").collection("products");
 
-  //POST Method
-  // app.post('/products', async(req, res) => {
-  //   const product = req.body;
-  //   if (!product.name || !product.price || !product.image || !product.stock) {
-  //     return res.send({success:false, error: "Please Provide all Info Correctly"});
-  //   }
-  //   await logicaCollection.insertOne(product);
-  //   res.send({success: true, message: `Successfully Product added ${product.name}`})
-  // });
+  // POST Method for new product add
+  app.post('/products', async(req, res) => {
+    const product = req.body;
+    if (!product.name || !product.price || !product.images || !product.stock) {
+      return res.send({success:false, error: "Please Provide all Info"});
+    }
+    await logicaCollection.insertOne(product);
+    res.send({success: true, message: `Successfully Product added ${product.name}`})
+  });
   
-//GET Method
+//GET Method for six product show of Home page
 app.get("/products", async(req, res) => {
-  // const items = parseInt(req.query.item);
-  // const pageNumber = parseInt(req.query.page);
   const cursor = logicaCollection.find({});
   const products = await cursor.limit(6).toArray();
-  const count = await logicaCollection.estimatedDocumentCount();
   if (!products?.length) {
     return res.send({success: false, error: "No Products Found"})
   }
-  res.send({success: true, data:products, count})
+  res.send({success: true, data:products})
 })
-
+//GET Method For find single product
+app.get('/products/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: ObjectId(id)};
+  const result = await logicaCollection.findOne(query);
+  res.send(result);
+})
+//GET method for find stock
+app.get("/allproducts", async(req, res) => {
+  const cursor = logicaCollection.find({});
+  const products = await cursor.toArray();
+  if (!products?.length) {
+    return res.send({success: false, error: "No Products Found"})
+  }
+  res.send({success: true, data:products})
+})
 
 
  }finally{
