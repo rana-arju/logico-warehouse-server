c4903c687d206ccb906731090712c27ec9a27f64
rana-arju <arjurana20@gmail.com>
2023-01-08 06:24:49 -0800

    update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const verifyToken = (req, res, next) => {
 });
 }
 
-const uri = `mongodb+srv://dbuser1:dbuser1@cluster0.ovwjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ovwjs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async () =>{
  try{
